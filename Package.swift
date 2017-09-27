2d07036d1ee5d4136d0b65c3f41ebb3d376b7f90
Alexander Shvets <alexander.shvets@gmail.com>
2017-09-27 17:00:19 -0400

    update spm version

--- Package.swift
+++ Package.swift
@@ -1,5 +1,25 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "Runglish"
+  name: "Runglish",
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "Runglish",
+      targets: ["Runglish"]),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .target(
+      name: "Runglish",
+      dependencies: [],
+      path: "Sources"),
+    .testTarget(
+      name: "RunglishTests",
+      dependencies: ["Runglish"]),
+  ]
 )
