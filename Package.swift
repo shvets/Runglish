5679f50b09f790ecb9a1e98f90b8054cd0ae1bf7
Alexander Shvets <alexander.shvets@gmail.com>
2020-01-20 20:51:11 -0500

    update project

--- Package.swift
+++ Package.swift
@@ -1,18 +1,19 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
   name: "Runglish",
+  platforms: [
+    .macOS(.v10_12),
+    .iOS(.v10),
+    .tvOS(.v10)
+  ],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
-    .library(
-      name: "Runglish",
-      targets: ["Runglish"]),
+    .library(name: "Runglish", targets: ["Runglish"]),
   ],
-  dependencies: [
-  ],
+  dependencies: [],
   targets: [
     .target(
       name: "Runglish",
